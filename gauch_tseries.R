aa528ba883574e47904c5d3048caf7f414a99a56
Edward Cai <edwsurewin@berkeley.edu>
2014-03-15 06:25:04 -0700

    GARCH_simu_demo

--- gauch_tseries.R
+++ gauch_tseries.R
@@ -51,3 +51,57 @@ _----
 ts.sim <- arima.sim(list(order = c(1,1,1), ar = 0.7, ma = 0.8), n = 200)
 ts.plot(ts.sim)
 
+--
+  
+  
+
+install.packages("fGarch")
+library(fGarch)
+
+## Univariate GARCH Time Series Fitting
+
+# example 1
+fit = garchFit(~garch(1, 1), data = garchSim())
+
+# example 2
+x.vec = as.vector(garchSim(garchSpec(rseed = 1985), n = 200)[,1])
+garchFit(~ garch(1,1), data = x.vec, trace = FALSE)
+
+--
+  
+## garchSim - Univariate GARCH/APARCH Time Series Simulation
+  
+## garchSpec - Univariate GARCH Time Series Speciﬁcation
+  
+  
+# GARCH(1,1) - specify omega/alpha/beta
+spec = garchSpec(model = list(omega = 1e-6, alpha = 0.1, beta = 0.8))
+garchSim(spec, n = 10)
+
+# AR([1,5])-GARCH(1,1) - use default garch values and subset ar[.]
+spec = garchSpec(model = list(mu = 0.001, ar = c(0.5,0,0,0,0.1)))
+garchSim(spec, n = 10)
+
+# ARMA(1,2)-GARCH(1,1) - use default garch values
+spec = garchSpec(model = list(ar = 0.5, ma = c(0.3, -0.3)))  
+garchSim(spec, n = 10)
+
+--
+
+## volatility-methods
+  
+## Swiss Pension func Index -
+x = as.timeSeries(data(LPP2005REC))
+## garchFit
+fit = garchFit(LPP40 ~ garch(1, 1), data = 100*x, trace = FALSE)
+fit
+## volatility -
+# Standard Deviation:
+volatility = volatility(fit, type = "sigma")
+head(volatility)
+class(volatility)
+# Variance:
+volatility = volatility(fit, type = "h")
+head(volatility)
+class(volatility)
+
